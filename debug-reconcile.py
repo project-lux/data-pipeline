5db075830a1ebb22fa4bd349d749c54a676e4f25
azaroth42 <azaroth42@gmail.com>
2024-04-30 12:15:55 -0400

    from/to

--- debug-reconcile.py
+++ debug-reconcile.py
@@ -19,106 +19,90 @@ cfgs.cache_globals()
 cfgs.instantiate_all()
 
 # --- process command line arguments ---
-### Give a YUID and calculate how the records got there
-yuids = []
-while '--recid' in sys.argv:
-    idx = sys.argv.index('--recid')
-    recid = sys.argv[idx+1]
-    sys.argv.pop(idx)
-    sys.argv.pop(idx)
+### Give from and to
 
-    if recid.startswith(cfgs.internal_uri):
-        # we're a YUID
-        yuids.append(recid)
-    else:
-        try:
-            (src, ident) = cfgs.split_uri(recid)
-        except:
-            print(f"Unknown URI: {recid}")
-            continue
-        try:
-            ref = src['mapper'].get_reference(ident)
-            base = cfgs.canonicalize(recid)
-            qua = cfgs.make_qua(base, ref.type)
-        except:
-            print(f"Could not make typed URI for {recid}")
-            continue
-        yuid = idmap[qua]
-        yuids.append(yuid)
+from_p = sys.argv[1]
+to_p = sys.argv[2]
 
-if not yuids:
-    print("No YUIDs/URIs able to be found")
-    sys.exit()
+try:
+    (src, ident) = cfgs.split_uri(from_p)
+except:
+    print(f"Unknown URI: {from_p}")
+    continue
+try:
+    ref = src['mapper'].get_reference(ident)
+    base = cfgs.canonicalize(recid)
+    qua = cfgs.make_qua(base, ref.type)
+except:
+    print(f"Could not make typed URI for {from_p}")
+    continue
+yuid = idmap[qua]
 
 # --- set up environment ---
 reconciler = Reconciler(cfgs, idmap, networkmap)
-debug = cfgs.debug_reconciliation
 
 curr = "A"
 idents = {}
 
-from_p = "https://media.art.yale.edu/content/lux/agt/31202.json"
-to_p = "http://vocab.getty.edu/ulan/500490811"
+uris = idmap[yuid]
 
-for yuid in yuids:
-    uris = idmap[yuid]
+# uri: [uris,that,are,connected]
+graph = {}
 
-    # uri: [uris,that,are,connected]
-    graph = {}
-
-    for u in uris:
-        if u.startswith('__'):
-            continue
-        (base, qua) = cfgs.split_qua(u)
-        (src, ident) = cfgs.split_uri(base)
-        idents[base] = f"{src['name']}:{curr}"
-        curr = chr(ord(curr)+1)
-        rec = src['acquirer'].acquire(ident)
-        if 'equivalent' in rec['data']:
-            for eq in rec['data']['equivalent']:
-                if 'id' in eq:
-                    eqid = eq['id']
-                    if not eqid in idents:
-                        try:
-                            (eqsrc, eqident) = cfgs.split_uri(eqid)
-                            idents[eqid] = f"{src['name']}:{curr}"
-                            curr = chr(ord(curr)+1)
-                        except:
-                            idents[eqid] = eqid
+for u in uris:
+    if u.startswith('__'):
+        continue
+    (base, qua) = cfgs.split_qua(u)
+    (src, ident) = cfgs.split_uri(base)
+    idents[base] = f"{src['name']}:{curr}"
+    curr = chr(ord(curr)+1)
+    rec = src['acquirer'].acquire(ident)
+    if 'equivalent' in rec['data']:
+        for eq in rec['data']['equivalent']:
+            if 'id' in eq:
+                eqid = eq['id']
+                if not eqid in idents:
                     try:
-                        graph[base].append(eq['id'])
+                        (eqsrc, eqident) = cfgs.split_uri(eqid)
+                        idents[eqid] = f"{src['name']}:{curr}"
+                        curr = chr(ord(curr)+1)
                     except:
-                        graph[base] = [eq['id']]
+                        idents[eqid] = eqid
+                try:
+                    graph[base].append(eq['id'])
+                except:
+                    graph[base] = [eq['id']]
 
-    G = nx.Graph()
-    G.add_nodes_from(list(idents.values()))
+G = nx.Graph()
+G.add_nodes_from(list(idents.values()))
 
-    new_graph = {}
-    for (k,v) in graph.items():
-        subj = idents[k]
-        l = []
-        for u in v:
-            if u in idents:
-                obj = idents[u]
-                l.append(obj)
-                G.add_edge(subj, obj)
-            else:
-                pass
-        l.sort()
-        new_graph[subj] = l
+new_graph = {}
+for (k,v) in graph.items():
+    subj = idents[k]
+    l = []
+    for u in v:
+        if u in idents:
+            obj = idents[u]
+            l.append(obj)
+            G.add_edge(subj, obj)
+        else:
+            pass
+    l.sort()
+    new_graph[subj] = l
 
 
-    key = []
-    for (k,v) in idents.items():
-        key.append((v, k))
+key = []
+for (k,v) in idents.items():
+    key.append((v, k))
 
-    key.sort()
-    print("  -- Key --")
-    for k in key:
-        print(f"  {k[0]:<16}{k[1]}")
-    print("")
-    print("Connected Nodes:")
-    print(list(nx.connected_components(G)))
-    print(nx.shortest_path(G, idents[from_p], idents[to_p]))
+key.sort()
+print("  -- Key --")
+for k in key:
+    print(f"  {k[0]:<16}{k[1]}")
+print("")
+print("Connected Nodes:")
+print(list(nx.connected_components(G)))
+print("Path from {from_p} to {to_p}")
+print(nx.shortest_path(G, idents[from_p], idents[to_p]))
 
  
