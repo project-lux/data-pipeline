eebc222624f27f547d634d45885a85506ba43ee6
Kelly Davis <kelly.davis@yale.edu>
2024-04-30 09:41:46 -0400

    step one testing

--- pipeline/sources/authorities/gbif/fetcher.py
+++ pipeline/sources/authorities/gbif/fetcher.py
@@ -1,7 +1,24 @@
 
 from pipeline.process.base.fetcher import Fetcher 
+import requests
 
 # URI is https://www.gbif.org/species/212
 # API is https://api.gbif.org/v1/species/212
 
+class GBIFFetcher(Fetcher):
+	def __init__(self, config):
+		Fetcher.__init__(self, config)
 
+		core = Fetcher.fetch(self, identifier) 
+		drec = Fetcher.fetch(self,f"{identifier}/descriptions")
+		srec = Fetcher.fetch(self,f"{identifier}/speciesProfiles")
+
+		print(core)
+		print(drec)
+		print(srec)
+
+#return {'data': data, 'source': self.name, 'identifier': identifier}
+
+        # #toss out useless descriptions
+        # if desc.startswith("Figs"):
+        #     continue
