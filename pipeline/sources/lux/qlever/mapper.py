420a25727a9e85ef4699ae7c0af16f34e3a74c62
azaroth42 <azaroth42@gmail.com>
2024-05-28 12:44:33 -0400

    conteNt

--- pipeline/sources/lux/qlever/mapper.py
+++ pipeline/sources/lux/qlever/mapper.py
@@ -493,11 +493,9 @@ class QleverMapper(Mapper):
         if 'identified_by' in data:
             for nm in data['identified_by']:
                 nmcxns = [y['id'] for y in nm.get('classified_as', []) if 'id' in y]
-                print(nmcxns)
-                if 'type' in nm and nm['type'] == 'Name' and self.globals['primaryName'] in nmcxns and 'context' in nm:
+                if 'type' in nm and nm['type'] == 'Name' and self.globals['primaryName'] in nmcxns and 'content' in nm:
                     t = {'subject': me, 'predicate': f"{self.luxns}primaryName", 'value': nm['content'], 'datatype':''}
                     triples.append(self.literal_pattern.format(**t))
-                    print(f"found primary name! {t}")
 
         # {pfx}Any
         for ref in conf['refs'].keys():
