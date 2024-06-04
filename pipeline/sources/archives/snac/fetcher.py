f88ae96671fe97613ae0a290c9aee26754a7a6f8
Kelly Davis <kelly.davis@yale.edu>
2024-06-04 15:35:37 -0400

    capitalize

--- pipeline/sources/archives/snac/fetcher.py
+++ pipeline/sources/archives/snac/fetcher.py
@@ -2,7 +2,6 @@ from pipeline.process.base.fetcher import Fetcher
 #we might want to increase the timeout time for SNAC as bigger records take longer to load, e.g. Yale w6r8240t
 
 
-class SnacFetcher(Fetcher):
-
-    def fetch(self, identifier):
-        pass
+class SNACFetcher(Fetcher):
+    def validate_identifier(self, identifier):
+        return True
