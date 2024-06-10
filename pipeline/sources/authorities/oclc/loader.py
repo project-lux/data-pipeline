9d55435d1e07ae3c3603591582ebc5d5b732ebe0
Kelly Davis <kelly.davis@yale.edu>
2024-06-10 14:34:32 -0400

    testing ident

--- pipeline/sources/authorities/oclc/loader.py
+++ pipeline/sources/authorities/oclc/loader.py
@@ -75,24 +75,25 @@ class FastLoader(ViafLoader):
             if not f.endswith(".marcxml"):
                 pass
             facet = fh.open(f)
-            #ident = f
-
+            
             l = facet.read()
             try:
                 facet.close()
             except:
                 pass
 
-            records = etree.fromstring(l).xpath('//mx:record')
+            ns = {'mx': 'http://www.loc.gov/MARC21/slim'}
+            records = etree.fromstring(l).xpath('//mx:record', namespaces=ns)
             for record in records:
                 record_str = etree.tostring(record).decode('utf-8')
-                print(record_str)
-                break
+                identfield = record.xpath('//mx:controlfield[@tag="001"]', namespaces=ns)
+                if controlfield:
+                    ident = controlfield[0].text
 
             x += 1
             done_x += 1
-            new = {"xml": xml}
-            self.out_cache[what] = new
+            new = {"xml": record_str}
+            self.out_cache[ident] = new
 
             if not done_x % 10000:
                 t = time.time() - start
