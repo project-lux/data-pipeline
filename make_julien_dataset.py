4e487e0bebb2f2a5668546372d0e1c1249f32874
azaroth42 <azaroth42@gmail.com>
2024-06-25 09:52:28 -0400

    record direct vs indirect

--- make_julien_dataset.py
+++ make_julien_dataset.py
@@ -15,7 +15,6 @@ cfgs.cache_globals()
 cfgs.instantiate_all()
 
 def _walk_rec(node, links):
-
     if 'id' in node:
         if 'vocab.getty.edu' in node['id']:
             links[node['id']] = 1    
@@ -52,19 +51,31 @@ def _walk_rec(node, links):
 def get_getty_links(rec):
     links = {}
     _walk_rec(rec['data'], links)
-    return list(links.keys())
+    direct = [x for x in links.keys() if links[x] == 1]
+    indirect = [x for x in links.keys() if links[x] == 2]
+    return (direct, indirect)
 
 cache = {}
 results = {}
 
+x = 0
+ttl = 151528 + 404099
 for src in ['ycba', 'yuag']:
     store = cfgs.internal[src]['recordcache']
     for rec in store.iter_records():
-        refs = get_getty_links(rec)
-        for r in refs:
+        (direct, indirect) = get_getty_links(rec)
+        for r in direct:
             if r in results:
                 results[r][src] = rec['identifier']
             else:
                 results[r] = {src: rec['identifier']}
-        sys.stdout.write('.');sys.stdout.flush()
+        for r in indirect:
+            if r in results:
+                # if both, only report direct
+                if not src in results[r] and not f"{src}_i" in results[r]:
+                    results[r][f"{src}_i"] = rec['identifier']
+            else:
+                results[r] = {f"{src}_i": rec['identifier']}
 
+        if not x % 10000:
+            print(f"{x} / {ttl}")
