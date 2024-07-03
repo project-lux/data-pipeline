6fd446074b7d75323457f86c27fbb91e28d3ff91
Kelly Davis <kelly.davis@yale.edu>
2024-07-03 16:03:55 -0400

    with valid creds and token

--- populate-timestamps.py
+++ populate-timestamps.py
@@ -1,38 +1,38 @@
-import os
-import sys
-import csv
-import json
-import time
-
-from google.oauth2.service_account import Credentials
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+import os
 
-
-from dotenv import load_dotenv
-from pipeline.config import Config
-
-load_dotenv()
-basepath = os.getenv('LUX_BASEPATH', "")
-cfgs = Config(basepath=basepath)
-idmap = cfgs.get_idmap()
-cfgs.cache_globals()
-cfgs.instantiate_all()
 
 directory = "/home/kd736/data-pipeline"
-
+creds = None
 gidfn = os.path.join(directory, 'populate-timestamps.txt')
 fh = open(gidfn)
 SPREADSHEET_ID = fh.read().strip()
 fh.close()
 
+directory = "/Users/kd736/Desktop/Development/data-pipeline/"
 scope = ['https://www.googleapis.com/auth/spreadsheets']
 
+tokfn = os.path.join(directory, 'token.json')
+credfn = os.path.join(directory, 'credentials.json')
+if os.path.exists(tokfn):
+    creds = Credentials.from_authorized_user_file(tokfn, scope)
 
-def populate_google_sheet(data):
-    scope = ['https://www.googleapis.com/auth/spreadsheets']
-    credfn = os.path.join(cfgs.data_dir, 'credentials.json')
-    creds = Credentials.from_service_account_file(credfn, scopes=scope)
-    
+# If there are no (valid) credentials available, let the user log in.
+if not creds or not creds.valid:
+    if creds and creds.expired and creds.refresh_token:
+    	creds.refresh(Request())
+    else:
+        flow = InstalledAppFlow.from_client_secrets_file(credfn, scope)
+        creds = flow.run_local_server(port=0)
+    # Save the credentials for the next run
+    with open(tokfn, 'w') as token:
+        token.write(creds.to_json())
+
+def populate_google_sheet(data):    
     service = build('sheets', 'v4', credentials=creds)
     sheet = service.spreadsheets()
     
