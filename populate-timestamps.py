e26ef2fb801f3f96777e8aef84fae3035c20ba34
Kelly Davis <kelly.davis@yale.edu>
2024-07-03 12:53:02 -0400

    modules, txt file

--- populate-timestamps.py
+++ populate-timestamps.py
@@ -9,8 +9,7 @@ from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
-from oauth2client.service_account import ServiceAccountCredentials
-import gspread
+
 
 from dotenv import load_dotenv
 from pipeline.config import Config
