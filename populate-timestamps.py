8b578c6e678a7b2d5430abece48cd4bba859210a
Kelly Davis <kelly.davis@yale.edu>
2024-07-05 11:34:40 -0400

    less dicts?

--- populate-timestamps.py
+++ populate-timestamps.py
@@ -82,15 +82,15 @@ def populate_google_sheet(data):
         body = {
             'values': [
             [
-                {'userEnteredValue': {'stringValue': 'Source'}}, {'userEnteredFormat': {'textFormat': {'bold': True}}},
-                {'userEnteredValue': {'stringValue': 'Timestamp'}}, {'userEnteredFormat': {'textFormat': {'bold': True}}},
-                {'userEnteredValue': {'stringValue': 'Internal or External?'}}, {'userEnteredFormat': {'textFormat': {'bold': True}}}
+                'userEnteredValue': {'stringValue': 'Source'}, 'userEnteredFormat': {'textFormat': {'bold': True}},
+                'userEnteredValue': {'stringValue': 'Timestamp'}, 'userEnteredFormat': {'textFormat': {'bold': True}},
+                'userEnteredValue': {'stringValue': 'Internal or External?'}, 'userEnteredFormat': {'textFormat': {'bold': True}}
             ]
             ] + [
             [
-                {'userEnteredValue': {'stringValue': cache}},
-                {'userEnteredValue': {'stringValue': data[cache]['timestamp']}},
-                {'userEnteredValue': {'stringValue': data[cache]['type']}}
+                'userEnteredValue': {'stringValue': cache},
+                'userEnteredValue': {'stringValue': data[cache]['timestamp']},
+                'userEnteredValue': {'stringValue': data[cache]['type']}
             ] for cache in data
             ]
         }
