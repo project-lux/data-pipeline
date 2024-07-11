762bfc20da05833292a149945f0a07db24f877d8
azaroth42 <azaroth42@gmail.com>
2024-07-10 21:48:31 -0400

    use only pre-cached

--- test-gemini-results.py
+++ test-gemini-results.py
@@ -39,82 +39,62 @@ test_res = []
 tsv = open("testing.tsv", "w")
 
 for r in results:
-    if "wp" in r:
+    if "wp" in r and "wp_wd" in r:
         wp = r["wp"]
         wpname = wp.rsplit("/", 1)[-1]
 
-        # fetch it from wikimedia and recache at end
-        if 0:  # run through current known
-            try:
-                lang = wp.replace("https://", "")
-                lang = lang.replace("http://", "")  # just in case
-                lang = lang.split(".")[0]
-                if lang != "en" or not "wp_wd" in r:
-                    print(f"Fetching: {wpname} from {lang}")
-                    resp = requests.get(wmuri.replace("{PAGENAME}", wpname).replace("{LANG}", lang))
-                    js = resp.json()
-                    r["wp_wd"] = js
-            except Exception as e:
-                print("Wikimedia API request raised exception:")
-                print(e)
+        qry = r["wp_wd"].get("query", {})
+        if "pages" in qry:
+            pg = list(qry["pages"].values())[0]
+            if "pageprops" in pg and "wikibase_item" in pg["pageprops"]:
+                wd = pg["pageprops"]["wikibase_item"]
+                # Now look to see if this item is connected back in LUX
+                cid = r["child_id"][0]
+                yuid = idmap[libns + cid + "##quaPlace"]
+                if yuid:
+                    equivs = idmap[yuid]
+                else:
+                    yuid = ""
+                    equivs = []
+                wduri = wdns + wd
 
-        if "wp_wd" in r:
-            qry = r["wp_wd"].get("query", {})
-            if "normalized" in qry:
-                title = qry["normalized"][0]["to"]
-            else:
-                title = ""
-            if "pages" in qry:
-                pg = list(qry["pages"].values())[0]
-                if "pageprops" in pg and "wikibase_item" in pg["pageprops"]:
-                    wd = pg["pageprops"]["wikibase_item"]
-                    # Now look to see if this item is connected back in LUX
-                    cid = r["child_id"][0]
-                    yuid = idmap[libns + cid + "##quaPlace"]
-                    if yuid:
-                        equivs = idmap[yuid]
-                    else:
-                        yuid = ""
-                        equivs = []
-                    wduri = wdns + wd
+                sys.stdout.write(".")
+                sys.stdout.flush()
 
-                    sys.stdout.write(".")
-                    sys.stdout.flush()
+                tr = [cid, r["child_id"][1], r.get("name", ""), wpname, wduri, yuid]
+                # print(f"{cid}/{r['child_id'][1]} = {r['wp']} = {wd}")
+                if wduri in equivs:
+                    tr.append("match")
+                    test_res.append(tr)
+                else:
+                    done = 0
+                    wyuid = idmap[wduri + "##quaPlace"]
+                    if wyuid:
+                        # print(f" ... Assigned YUID: {wyuid}")
+                        uu = wyuid.rsplit("/", 1)[-1]
+                        luxrec = merged[uu]
+                        if luxrec:
+                            names = [x["content"] for x in luxrec["data"]["identified_by"] if x["type"] == "Name"]
+                            # print(f" ... LUX name 1: {names[0]} ")
+                            tr.append("other")
+                            tr.append(uu)
+                            tr.append(names[0])
+                            test_res.append(tr)
+                            done = 1
+                    if not done:
+                        # print(f" ... Unknown WD entry")
+                        wdrec = wd_acq.acquire(wd, rectype="Place")
+                        if wdrec:
+                            names = [x["content"] for x in wdrec["data"]["identified_by"]]
+                            # print(f" ... WD Name 1: {names[0]}")
+                            tr.append("not seen")
+                            tr.append("")
+                            tr.append(names[0])
+                            test_res.append(tr)
+                        else:
+                            tr.append("invalid")
 
-                    tr = [cid, r["child_id"][1], r.get("name", ""), wpname, title, wduri, yuid]
-                    # print(f"{cid}/{r['child_id'][1]} = {r['wp']} = {wd}")
-                    if wduri in equivs:
-                        tr.append("match")
-                        test_res.append(tr)
-                    else:
-                        done = 0
-                        wyuid = idmap[wduri + "##quaPlace"]
-                        if wyuid:
-                            # print(f" ... Assigned YUID: {wyuid}")
-                            uu = wyuid.rsplit("/", 1)[-1]
-                            luxrec = merged[uu]
-                            if luxrec:
-                                names = [x["content"] for x in luxrec["data"]["identified_by"] if x["type"] == "Name"]
-                                # print(f" ... LUX name 1: {names[0]} ")
-                                tr.append("other")
-                                tr.append(uu)
-                                tr.append(names[0])
-                                test_res.append(tr)
-                                done = 1
-                        if not done:
-                            # print(f" ... Unknown WD entry")
-                            wdrec = wd_acq.acquire(wd, rectype="Place")
-                            if wdrec:
-                                names = [x["content"] for x in wdrec["data"]["identified_by"]]
-                                # print(f" ... WD Name 1: {names[0]}")
-                                tr.append("not seen")
-                                tr.append("")
-                                tr.append(names[0])
-                                test_res.append(tr)
-                            else:
-                                tr.append("invalid")
-
-                    tsv.write("\t".join(tr))
-                    tsv.write("\n")
-                    tsv.flush()
+                tsv.write("\t".join(tr))
+                tsv.write("\n")
+                tsv.flush()
 tsv.close()
