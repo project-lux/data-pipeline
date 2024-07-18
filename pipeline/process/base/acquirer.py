78db6fa44b7764f6065b4dfa7e52f04d056e8c48
azaroth42 <azaroth42@gmail.com>
2024-07-18 09:51:05 -0400

    tidy

--- pipeline/process/base/acquirer.py
+++ pipeline/process/base/acquirer.py
@@ -1,24 +1,24 @@
-
 class Acquirer(object):
-
     def __init__(self, config):
         self.config = config
-        self.configs = config['all_configs']
-        self.datacache = config['datacache']
-        self.recordcache = config['recordcache']
-        self.mapper = config['mapper']
-        self.fetcher = config['fetcher']
-        self.debug = config.get('debug', 0)
-        self.name = config['name']
-        self.force_rebuild = config.get('force_rebuild', False)
-        self.validate = config.get('validate_on_acquire', False)
-        self.raise_on_error = config.get('raise_on_validation_error', False)
-        self.validator = config['all_configs'].validator
-        self.ignore_sources = config.get('ignore_sources_for_records', [])
+        self.configs = config["all_configs"]
+        self.datacache = config["datacache"]
+        self.recordcache = config["recordcache"]
+        self.mapper = config["mapper"]
+        self.fetcher = config["fetcher"]
+        self.debug = config.get("debug", 0)
+        self.name = config["name"]
+        self.force_rebuild = config.get("force_rebuild", False)
+        self.validate = config.get("validate_on_acquire", False)
+        self.raise_on_error = config.get("raise_on_validation_error", False)
+        self.validator = config["all_configs"].validator
+        self.ignore_sources = config.get("ignore_sources_for_records", [])
 
     def __getitem__(self, identifier):
         # allow rec = acquirer['ident'] code pattern
-        return self.acquire(identifier)
+        # allow acquirer[ident##quaType] to set rectype
+        params = self.configs.split_qua(identifier)
+        return self.acquire(*params)
 
     def returns_multiple(self, record=None):
         return self.mapper.returns_multiple(record)
@@ -31,29 +31,32 @@ class Acquirer(object):
                 return rec
 
         # Don't have it, and config says to ignore
+        # XXX Shouldn't this just be "enabled" per source?
         if self.name in self.ignore_sources:
-            if self.debug: print(f"       Skipping {self.name}/{identifier} in acquire due to ignore_sources")
+            if self.debug:
+                print(f"       Skipping {self.name}/{identifier} in acquire due to ignore_sources")
             return None
 
         if self.fetcher.enabled:
             rec = self.fetcher.fetch(identifier)
             if rec is None:
-                if self.debug: print(f"Got no record fetching {self.name}/{identifier}; skipping")
-                return None                    
-            if 'identifier' in rec and rec['identifier'] != identifier:
+                if self.debug:
+                    print(f"Got no record fetching {self.name}/{identifier}; skipping")
+                return None
+            if "identifier" in rec and rec["identifier"] != identifier:
                 # Might have been redirected
-                identifier = rec['identifier']
+                identifier = rec["identifier"]
             if store:
                 self.datacache[identifier] = rec
         else:
             # don't have it, can't fetch it...
-            if self.debug: print(f"Cannot fetch {self.name}/{identifier}; skipping")
+            if self.debug:
+                print(f"Fetcher for {self.name}/{identifier} disabled; skipping")
             return None
         return rec
 
-
     def do_post_map(self, rec, rectype, store):
-        identifier = rec['identifier']
+        identifier = rec["identifier"]
         rec3 = self.mapper.post_mapping(rec, rectype)
         if rec3:
             if self.validate:
@@ -67,18 +70,19 @@ class Acquirer(object):
                     else:
                         return None
             if store:
-                if self.config['type'] == 'external':
+                if self.config["type"] == "external":
                     try:
-                        qrecid = self.configs.make_qua(rec3['identifier'], rectype)    
+                        qrecid = self.configs.make_qua(rec3["identifier"], rectype)
                     except:
                         print(f"{rec3['identifier']} in {self.config['name']} has no type?? {rec3['data']}")
                         raise
-                    rec3['identifier'] = qrecid
+                    rec3["identifier"] = qrecid
                     self.recordcache[qrecid] = rec3
                 else:
                     self.recordcache[identifier] = rec3
         else:
-            if self.debug: print(f"Post Mapping killed {rectype} record {self.name}/{identifier}")
+            if self.debug:
+                print(f"Post Mapping killed {rectype} record {self.name}/{identifier}")
         return rec3
 
     def acquire_all(self, identifier, store=True):
@@ -88,7 +92,7 @@ class Acquirer(object):
         recs = self.mapper.transform_all(data)
         result = []
         for rec in recs:
-            result.append(self.do_post_map(rec, rec['data']['type'], store))
+            result.append(self.do_post_map(rec, rec["data"]["type"], store))
         return result
 
     def acquire(self, identifier, rectype=None, dataonly=False, store=True, reference=False):
@@ -97,7 +101,8 @@ class Acquirer(object):
 
         # validate identifier
         if self.fetcher and not self.fetcher.validate_identifier(identifier):
-            if self.debug: print(f"{identifier} is not a valid identifier for {self.name}")
+            if self.debug:
+                print(f"{identifier} is not a valid identifier for {self.name}")
             return None
 
         # Return already built record
@@ -105,7 +110,7 @@ class Acquirer(object):
             rec = self.recordcache[identifier]
             if rec is not None:
                 return rec
-            elif rectype is not None and self.config['type'] == 'external':
+            elif rectype is not None and self.config["type"] == "external":
                 qrecid = self.configs.make_qua(identifier, rectype)
                 rec = self.recordcache[qrecid]
                 if rec is not None:
@@ -115,18 +120,18 @@ class Acquirer(object):
         if dataonly or rec is None:
             return rec
 
-        identifier = rec['identifier'] # might have mutated during fetch
-        if rectype is None and 'type' in rec['data']:
-            if type(rec['data']['type']) == list:
-                rec['data']['type'].remove('Type') # Prefer Language/Material to Type
-                rec['data']['type'] = rec['data']['type'][0] # and pick the first, right or wrong
-            if type(rec['data']['type']) == dict:
-                print(rec['identifier'])
-                print(rec['data']['type'])
+        identifier = rec["identifier"]  # might have mutated during fetch
+        if rectype is None and "type" in rec["data"]:
+            if type(rec["data"]["type"]) == list:
+                rec["data"]["type"].remove("Type")  # Prefer Language/Material to Type
+                rec["data"]["type"] = rec["data"]["type"][0]  # and pick the first, right or wrong
+            if type(rec["data"]["type"]) == dict:
+                print(rec["identifier"])
+                print(rec["data"]["type"])
                 return None
 
-            if rec['data']['type'] in self.configs.ok_record_types:    
-                rectype = rec['data']['type']
+            if rec["data"]["type"] in self.configs.ok_record_types:
+                rectype = rec["data"]["type"]
 
         try:
             rec2 = self.mapper.transform(rec, rectype, reference=reference)
@@ -135,12 +140,13 @@ class Acquirer(object):
             return None
 
         if rec2 is None:
-            if self.debug: print(f"Cannot map {self.name}/{identifier} into Linked Art (as {rectype})")
+            if self.debug:
+                print(f"Cannot map {self.name}/{identifier} into Linked Art (as {rectype})")
             return None
-        rec2['identifier'] = identifier 
+        rec2["identifier"] = identifier
 
         if reference:
             return rec2
 
-        rec3 = self.do_post_map(rec2, rec2['data']['type'], store=store)
+        rec3 = self.do_post_map(rec2, rec2["data"]["type"], store=store)
         return rec3
--- pipeline/process/reference_manager.py
+++ pipeline/process/reference_manager.py
@@ -1,32 +1,31 @@
-
 import os
 import time
 import ujson as json
 
-class ReferenceManager(object):
 
+class ReferenceManager(object):
     def __init__(self, configs, idmap):
         self.configs = configs
         self.metatypes_seen = {}
-        self.all_refs = configs.instantiate_map('all_refs')['store']
-        self.done_refs = configs.instantiate_map('done_refs')['store']
+        self.all_refs = configs.instantiate_map("all_refs")["store"]
+        self.done_refs = configs.instantiate_map("done_refs")["store"]
         self.idmap = idmap
         self.debug = configs.debug_reconciliation
 
         self.internal_uris = [configs.internal_uri]
         for c in configs.internal.values():
-            self.internal_uris.append(c['namespace'])
+            self.internal_uris.append(c["namespace"])
 
         # XXX FIXME: This should be a CSV or sane JSON
-        fh = open(os.path.join(configs.data_dir,'replacements.json'))
+        fh = open(os.path.join(configs.data_dir, "replacements.json"))
         data = fh.read()
         fh.close()
         js = json.loads(data)
         getty_redirects = {}
-        res = js['results']['bindings']
+        res = js["results"]["bindings"]
         for r in res:
-            f = r['from']['value']
-            t = r['to']['value']
+            f = r["from"]["value"]
+            t = r["to"]["value"]
             getty_redirects[f] = t
         self.redirects = getty_redirects
         self.ref_cache = {}
@@ -37,37 +36,37 @@ class ReferenceManager(object):
             fn = f"metatypes-{my_slice}.json"
         else:
             fn = f"metatypes-single.json"
-        fh = open(fn, 'w')
+        fh = open(fn, "w")
         fh.write(json.dumps(self.metatypes_seen))
-        fh.close()        
+        fh.close()
 
     def write_done_refs(self):
         # step through all entries in done_refs and write URI
         # to a file, if distance <= MAX_DISTANCE
         maxd = self.configs.max_distance
-        fh = open('reference_uris.txt', 'w')
+        fh = open("reference_uris.txt", "w")
         x = 0
         for k in self.done_refs.iter_keys():
             x += 1
             if not x % 100000:
                 print(x)
-            if k['dist'] <= maxd:
+            if k["dist"] <= maxd:
                 fh.write(f"{k['dist']}|{k.pkey}\n")
         fh.close()
 
     def iter_done_refs(self, my_slice, max_slice):
-        fh = open('reference_uris.txt', 'r')
+        fh = open("reference_uris.txt", "r")
         if my_slice < 0 or max_slice < 0:
-            # just read the whole file            
+            # just read the whole file
             line = fh.readline()
             line = line.strip()
-            line = line.split('|')
+            line = line.split("|")
             while line:
                 yield line
                 line = fh.readline()
                 line = line.strip()
                 if line:
-                    line = line.split('|')
+                    line = line.split("|")
         else:
             okay = True
             while okay:
@@ -76,7 +75,7 @@ class ReferenceManager(object):
                 if not uri:
                     okay = False
                 else:
-                    uri = uri.split('|')
+                    uri = uri.split("|")
                     yield uri
         fh.close()
 
@@ -87,32 +86,31 @@ class ReferenceManager(object):
         return self.done_refs.popitem()
 
     def did_ref(self, uri, distance):
-        self.done_refs[uri] = {'dist': distance}
+        self.done_refs[uri] = {"dist": distance}
 
-    def delete_done_ref(self, eq):  
+    def delete_done_ref(self, eq):
         try:
-            del self.done_refs[eq]        
+            del self.done_refs[eq]
         except:
             # didn't exist anyway
             pass
 
     # type is needed for Concepts, as the qua is Type but the type is Material (etc)
     # a ref is {'dist': int, 'type': str}
-    def add_ref(self, ref, refs, distance, ctype): 
-
+    def add_ref(self, ref, refs, distance, ctype):
         if ref in self.ref_cache:
             return None
 
         xr = self.all_refs[ref]
         if xr is not None:
-            xdist = xr['dist']
-            xctype = xr['type']
+            xdist = xr["dist"]
+            xctype = xr["type"]
         else:
             xdist = None
-            xctype = None            
+            xctype = None
         dref = self.done_refs[ref]
         if dref is not None:
-            ddist = dref['dist']
+            ddist = dref["dist"]
         else:
             ddist = None
 
@@ -121,33 +119,33 @@ class ReferenceManager(object):
             if ddist is not None and ddist > distance:
                 # need to re-add it to all with new distance
                 del self.done_refs[ref]
-                self.all_refs[ref] = {'dist': distance, 'type': ctype}
+                self.all_refs[ref] = {"dist": distance, "type": ctype}
             elif xdist is not None and distance < xdist:
                 # in all, not in done, less distance: update in all
-                xr['dist'] = distance
+                xr["dist"] = distance
             if not xctype and ctype:
-                xr['type'] = ctype
+                xr["type"] = ctype
         elif dref is not None:
             # Test Distance
             if ddist is not None and ddist > distance:
                 # Add it back in
                 del self.done_refs[ref]
-                self.all_refs[ref] = {'dist': distance, 'type': ctype}
+                self.all_refs[ref] = {"dist": distance, "type": ctype}
         elif not ref in refs:
-            val = {'dist': distance, 'type': ctype}
+            val = {"dist": distance, "type": ctype}
             refs[ref] = val
-            self.all_refs[ref] = val 
+            self.all_refs[ref] = val
             if distance == 1 and "vocab.getty.edu/aat" in ref:
                 self.ref_cache[ref] = distance
 
     def walk_for_refs(self, node, refs, distance, top=False):
         # Test if we need to record the node
 
-        if not top and 'id' in node and not node['id'].startswith('_'):
-            if node['id'] in self.redirects:
-                node['id'] = self.redirects[node['id']]
+        if not top and "id" in node and not node["id"].startswith("_"):
+            if node["id"] in self.redirects:
+                node["id"] = self.redirects[node["id"]]
 
-            val = self.configs.make_qua(node['id'], node['type'])
+            val = self.configs.make_qua(node["id"], node["type"])
             should_add_ref = True
             for i in self.internal_uris:
                 if val.startswith(i):
@@ -156,21 +154,21 @@ class ReferenceManager(object):
                     should_add_ref = False
                     break
             if should_add_ref:
-                t = node.get('type', '')
+                t = node.get("type", "")
                 ct = t if t in self.configs.parent_record_types else ""
                 self.add_ref(val, refs, distance, ct)
 
             # but still want to save meta-types
-            if (node['type'] in self.configs.parent_record_types or node['type'] == 'Type') and 'classified_as' in node:
-                cxids = [x['id'] for x in node['classified_as'] if 'id' in x]
-                if not node['id'] in self.metatypes_seen:
-                    self.metatypes_seen[node['id']] = []            
+            if (node["type"] in self.configs.parent_record_types or node["type"] == "Type") and "classified_as" in node:
+                cxids = [x["id"] for x in node["classified_as"] if "id" in x]
+                if not node["id"] in self.metatypes_seen:
+                    self.metatypes_seen[node["id"]] = []
                 for cx in cxids:
-                    if not cx in self.metatypes_seen[node['id']]:
-                        self.metatypes_seen[node['id']].append(cx)
+                    if not cx in self.metatypes_seen[node["id"]]:
+                        self.metatypes_seen[node["id"]].append(cx)
 
-        for (k,v) in node.items():
-            if k in ['equivalent', 'access_point', 'conforms_to']:
+        for k, v in node.items():
+            if k in ["equivalent", "access_point", "conforms_to"]:
                 continue
             if type(v) == list:
                 for vi in v:
@@ -179,19 +177,18 @@ class ReferenceManager(object):
             elif type(v) == dict:
                 self.walk_for_refs(v, refs, distance)
 
-
     def walk_top_for_refs(self, rec, distance):
         refs = {}
         if rec is None:
             return refs
-        if 'data' in rec:
-            rec = rec['data']
-        if not 'id' in rec:
+        if "data" in rec:
+            rec = rec["data"]
+        if not "id" in rec:
             return {}
 
         try:
-            self.walk_for_refs(rec, refs, distance+1, top=True)
-        except ValueError as e:        
+            self.walk_for_refs(rec, refs, distance + 1, top=True)
+        except ValueError as e:
             print(f"\nERROR: Reference walk error in {rec['id']}: {e}")
             raise
 
@@ -203,9 +200,9 @@ class ReferenceManager(object):
         # equivalents of internal records will be distance 0...
         # could detect that way?
 
-        if 'equivalent' in rec:
-            for eq in rec['equivalent']:
-                k = self.configs.make_qua(eq['id'], rec['type'])
+        if "equivalent" in rec:
+            for eq in rec["equivalent"]:
+                k = self.configs.make_qua(eq["id"], rec["type"])
                 should_add_ref = True
                 for i in self.internal_uris:
                     if k.startswith(i):
@@ -214,19 +211,18 @@ class ReferenceManager(object):
                         should_add_ref = False
                         break
                 if should_add_ref:
-                    t = rec.get('type', '')
+                    t = rec.get("type", "")
                     ct = t if t in self.configs.parent_record_types else ""
                     self.add_ref(k, refs, distance, ct)
 
         return refs
 
-
     def manage_identifiers(self, rec):
-        if not rec or not 'data' in rec or not 'id' in rec['data']:
+        if not rec or not "data" in rec or not "id" in rec["data"]:
             return
-        recid = rec['data']['id']
-        typ = rec['data']['type']
-        equivs = [x['id'] for x in rec['data'].get('equivalent', [])]
+        recid = rec["data"]["id"]
+        typ = rec["data"]["type"]
+        equivs = [x["id"] for x in rec["data"].get("equivalent", [])]
         qequivs = [self.configs.make_qua(x, typ) for x in equivs]
 
         # This should be called after ALL reconciliation processing has happened
@@ -240,15 +236,17 @@ class ReferenceManager(object):
         uu = self.idmap[qrecid]
         if uu is not None:
             # We know about this entity/record already
-            if self.debug: print(f"Found {uu} for {qrecid}")
+            if self.debug:
+                print(f"Found {uu} for {qrecid}")
             equiv_map[qrecid] = uu
             uuset = self.idmap[uu]
             if uuset:
                 existing = list(uuset)
-                if self.debug: print(f"Found existing: {existing}")
+                if self.debug:
+                    print(f"Found existing: {existing}")
         else:
-            if self.debug: print(f"Got None for {qrecid}, will mint or find")
-
+            if self.debug:
+                print(f"Got None for {qrecid}, will mint or find")
 
         updated_token = False
         # if we have the current update token, then we've already been touched
@@ -262,7 +260,8 @@ class ReferenceManager(object):
         # Ensure that previous bad reconciliations are undone
         # But only the first time we see this uuid
         if uu and rebuild:
-            if self.debug: print("No update token!")
+            if self.debug:
+                print("No update token!")
             self.idmap.add_update_token(uu)
             updated_token = True
             if existing:
@@ -270,18 +269,21 @@ class ReferenceManager(object):
                 to_delete = []
                 for x in existing.copy():
                     if not x in qequivs:
-                        if self.debug: print(f"Removing {x} not in new equivs")
+                        if self.debug:
+                            print(f"Removing {x} not in new equivs")
                         existing.remove(x)
                         if not x.startswith("__"):
                             try:
                                 del self.idmap[x]
-                                if self.debug: print(f"deleted {x}")
+                                if self.debug:
+                                    print(f"deleted {x}")
                             except:
                                 print(f"\nWhile processing {recid} found {equivs} in record")
                                 print(f"Tried to delete {x} for {uu}")
                     else:
-                        if self.debug: print(f"Found {x} in existing and new")
-          
+                        if self.debug:
+                            print(f"Found {x} in existing and new")
+
         # Build map of equivalent ids given in current record
         if equivs:
             for eq in equivs.copy():
@@ -290,18 +292,19 @@ class ReferenceManager(object):
                     myqeq = self.idmap[qeq]
                     if myqeq is not None:
                         equiv_map[eq] = myqeq
-                    if self.debug: print(f"qeq: {qeq} / {myqeq}")
+                    if self.debug:
+                        print(f"qeq: {qeq} / {myqeq}")
 
         # Ensure existing from idmap are in equivalent map
         # This will only make changes on second and subsequent times
         # we encounter the YUID
         if existing:
             for xq in existing.copy():
-                if not xq.startswith('__'):
+                if not xq.startswith("__"):
                     equiv_map[xq] = uu
 
         # It is possible that equiv_map contains multiple YUIDS
-        # And we will need to merge 
+        # And we will need to merge
         if not equiv_map:
             # Don't know anything at all, ask for a new yuid
             slug = self.configs.ok_record_types.get(typ, None)
@@ -311,16 +314,17 @@ class ReferenceManager(object):
             uu = self.idmap.mint(qrecid, slug)
             self.idmap.add_update_token(uu)
             updated_token = True
-            if self.debug: print(f"Minted {slug}/{uu} for {qrecid} ")
+            if self.debug:
+                print(f"Minted {slug}/{uu} for {qrecid} ")
 
             for eq in equivs:
                 qeq = self.configs.make_qua(eq, typ)
                 try:
                     self.idmap[qeq] = uu
                 except:
-                    print(f"\nERROR: Failed to set {qeq} as yuid: {uu} for {qrecid} having just minted it?")                
+                    print(f"\nERROR: Failed to set {qeq} as yuid: {uu} for {qrecid} having just minted it?")
 
-        else:                
+        else:
             # We have something from the data and/or previous build
             uul = list(equiv_map.values())
             uus = set(uul)
@@ -332,7 +336,8 @@ class ReferenceManager(object):
                 if not qrecid in equiv_map:
                     # e.g. second occurence of Wiley painting
                     try:
-                        if self.debug: print(f"Setting {qrecid} to {uu} as uus=1")
+                        if self.debug:
+                            print(f"Setting {qrecid} to {uu} as uus=1")
                         self.idmap[qrecid] = uu
                     except:
                         print(f"Failed to set {qrecid} to {uu} from {equiv_map} / {uus}")
@@ -352,7 +357,7 @@ class ReferenceManager(object):
                                 src, recid = self.configs.split_uri(i)
                             except:
                                 continue
-                            if src['type'] == 'internal':
+                            if src["type"] == "internal":
                                 internals.append([u, uul.count(u)])
                             else:
                                 externals.append([u, uul.count(u)])
@@ -377,7 +382,7 @@ class ReferenceManager(object):
                     if existing_ud:
                         for eqd in existing_ud:
                             if not eqd.startswith("__"):
-                                self.idmap.delete(eqd) 
+                                self.idmap.delete(eqd)
                                 self.idmap[eqd] = uu
 
         # Ensure we touch the token
@@ -392,7 +397,9 @@ class ReferenceManager(object):
                     qeq = self.configs.make_qua(eq, typ)
                 else:
                     qeq = eq
-                if self.debug: print(f"Setting {qeq} to {uu} in idmap")
+                if self.debug:
+                    print(f"Setting {qeq} to {uu} in idmap")
                 self.idmap[qeq] = uu
             else:
-                if self.debug: print(f"Saw {eq} in existing, not setting")
+                if self.debug:
+                    print(f"Saw {eq} in existing, not setting")
