c4bff4bc5bda4e446463a3a08053982949d3a452
azaroth42 <azaroth42@gmail.com>
2024-07-24 09:08:33 -0400

    test

--- pipeline/sources/authorities/lc/mapper.py
+++ pipeline/sources/authorities/lc/mapper.py
@@ -789,6 +789,7 @@ class LcnafMapper(LcMapper):
                     gid = self.build_recs_and_reconcile(lbl, "group")
                 if gid:
                     fetchid = gid.rsplit("/", 1)[-1]
+                    print(fetchid)
                     frec = self.get_reference(fetchid)
                     if frec.__class__ == model.Group:
                         if topcls in [model.Group, model.Person]:
