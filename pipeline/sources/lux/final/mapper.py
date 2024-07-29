103ec08a7adedda41a4d1d8d3ca25104c78395e3
azaroth42 <azaroth42@gmail.com>
2024-07-29 10:52:48 -0400

    -name debug

--- pipeline/sources/lux/final/mapper.py
+++ pipeline/sources/lux/final/mapper.py
@@ -189,8 +189,6 @@ class Cleaner(Mapper):
 
             # lang_names = {None: [{pn}, {sn}]}
 
-            print(f"lang_names: {lang_names}")
-
             for lang, nms in lang_names.items():
                 has_sort = False
                 primaryNameVals = []
