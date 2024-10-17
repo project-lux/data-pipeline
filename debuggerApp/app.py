e9ce16a3c43973d5b0eecc963a30be4264a6c9d1
Kelly Davis <kelly.davis@yale.edu>
2024-10-17 10:28:52 -0400

    towards checkboxes to add to google sheet

--- debuggerApp/app.py
+++ debuggerApp/app.py
@@ -1,5 +1,47 @@
 from flask import Flask, render_template, request
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
 from debugger import process_uri
+
+from dotenv import load_dotenv
+from pipeline.config import Config
+
+load_dotenv()
+basepath = os.getenv('LUX_BASEPATH', "")
+cfgs = Config(basepath=basepath)
+idmap = cfgs.get_idmap()
+cfgs.cache_globals()
+cfgs.instantiate_all()
+
+directory = cfgs.data_dir
+creds = None
+gidfn = os.path.join(cfgs.data_dir, 'google_sheet_id.txt')
+fh = open(gidfn)
+SPREADSHEET_ID = fh.read().strip()
+fh.close()
+
+SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly']
+
+tokfn = os.path.join(cfgs.data_dir, 'token.json')
+credfn = os.path.join(cfgs.data_dir, 'credentials.json')
+if os.path.exists(tokfn):
+    creds = Credentials.from_authorized_user_file(tokfn, SCOPES)
+
+if not creds or not creds.valid:
+    if creds and creds.expired and creds.refresh_token:
+        creds.refresh(Request())
+    else:
+        flow = InstalledAppFlow.from_client_secrets_file(credfn, SCOPES)
+        creds = flow.run_local_server(port=0)
+    # Save the credentials for the next run
+    with open(tokfn, 'w') as token:
+        token.write(creds.to_json())
+
+service = build('sheets', 'v4', credentials=creds)
+sheet = service.spreadsheets()
 
 app = Flask(__name__)
 @app.route('/', methods=['GET','POST'])
@@ -22,6 +64,17 @@ def index():
         #pass result to template
         return render_template('result.html', result=result)
     return render_template("index.html")
+
+@app.route('/add_to_sheet', methods=['POST'])
+def add_to_sheet():
+    equivalents_A = request.form.getlist('equivalent_A')
+    equivalents_B = request.form.getlist('equivalent_B')
+
+    # Assuming that pairs of A and B are to be added to the sheet
+    for equiv_A, equiv_B in zip(equivalents_A, equivalents_B):
+        sheet.append_row([equiv_A, equiv_B])  # Add the pair to columns A and B
+
+    return "Selected equivalent pairs added to Google Sheet!"
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", debug=True, port=8080)
