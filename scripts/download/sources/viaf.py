8eafe14b0f7e429cde9cdcd6147674a4af4c6283
wjm55 <william.mattingly@yale.edu>
2024-12-09 10:55:08 -0500

    refactor to align with 0.0.3

--- scripts/download/sources/viaf.py
+++ scripts/download/sources/viaf.py
@@ -1,30 +1,3 @@
-"""
-This script is designed to fetch the VIAF data page, extract the first `resource` URL
-that matches the specific pattern:
-`https://viaf.org/viaf/data/viaf-NNNNNNNN-clusters.xml.gz`, where `NNNNNNNN` represents
-a variable numeric identifier, and print the URL.
-
-The script consists of two main functions:
-
-1. **parse_viaf(html_content):**
-   - Parses the fetched HTML content using `BeautifulSoup`.
-   - Searches within the `<article class="data-files">` section for `<dl>` elements with
-     a `resource` attribute.
-   - Validates that the `resource` attribute matches the required URL pattern using
-     regular expressions.
-   - Returns the first matching URL or `None` if no match is found.
-
-2. **main():**
-   - Uses the imported fetch_webpage function to fetch the webpage.
-   - Then calls the parse_viaf function  to parse the webpage.
-   - Prints the first valid `resource` URL matching the pattern or an appropriate
-     error message if no valid URL is found.
-   - Returns the valid url.
-
-## Example usage:
-url=$(python scripts/download/viaf.py)
-curl -O "$url"
-"""
 import sys
 import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../../..')))
@@ -34,7 +7,33 @@ from pipeline.sources.utils import fetch_webpage
 
 
 def main():
-    """Main function to fetch and parse file URL from VIAF data page."""
+    """
+    This script is designed to fetch the VIAF data page, extract the first `resource` URL
+    that matches the specific pattern:
+    `https://viaf.org/viaf/data/viaf-NNNNNNNN-clusters.xml.gz`, where `NNNNNNNN` represents
+    a variable numeric identifier, and print the URL.
+
+    The script consists of two main functions:
+
+    1. **parse_viaf(html_content):**
+    - Parses the fetched HTML content using `BeautifulSoup`.
+    - Searches within the `<article class="data-files">` section for `<dl>` elements with
+        a `resource` attribute.
+    - Validates that the `resource` attribute matches the required URL pattern using
+        regular expressions.
+    - Returns the first matching URL or `None` if no match is found.
+
+    2. **main():**
+    - Uses the imported fetch_webpage function to fetch the webpage.
+    - Then calls the parse_viaf function  to parse the webpage.
+    - Prints the first valid `resource` URL matching the pattern or an appropriate
+        error message if no valid URL is found.
+    - Returns the valid url.
+
+    ## Example usage:
+    url=$(python scripts/download/viaf.py)
+    curl -O "$url"
+    """
     url = "https://viaf.org/viaf/data/"
     html_content = fetch_webpage(url)
     if html_content:
