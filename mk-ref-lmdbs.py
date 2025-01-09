ab5ee1a5fbfe9a5b8a55999b0d8570cd4c73585d
Rob Sanderson <azaroth@gmail.com>
2025-01-09 16:06:11 -0500

    experiment with lmdb index of references

--- mk-ref-lmdbs.py
+++ mk-ref-lmdbs.py
@@ -13,9 +13,27 @@ cfgs.instantiate_all()
 # Exclude globals
 gls = set([x[-36:] for x in list(cfgs.globals.values()) if x])
 
+non_global_excludes = [
+    '2eca07bd-be42-4ef5-9ec5-87c1bbfe639d', # web pages
+    '89630361-18a3-4c4b-bcd7-16894d95defd', # record identifiers
+    'd07b9b86-0a1e-4026-aa4c-8ecba8bbd9c9', # YUL group
+    '03f4eb19-0611-4f31-8e09-fc111c52f898', # access statement type
+    'd97f2e4a-1549-4c4b-9a27-bcf41a2b4972', # visitors (?) statement type
+    '91fd5ce0-b58a-4571-9fde-839aed0c876e', # physical description
+    '53922f57-dab5-43c5-a527-fc20a63fe128', # dimensions description
+    'f3e231d7-6b87-4af2-9bb8-e7b0b1f6ec09', # imprints
+    '35961a03-7a62-4494-bd50-f1630477035f', # call numbers
+    'a1922667-5c32-4181-8897-3e71ecea0ce8', # repository numbers
+    'b0ac44c2-83ba-4eae-9a01-fcfa547dd5b0', # numerals (?)
+    'acc79f08-f8f7-4f7c-8179-1c8f03aa9020', # notes
+    'fcd4a80f-b3e2-4c56-9153-3e3a4f4dc19d', # transcription
+    '18b96dab-a9f1-4b30-83d9-599289e490ba', # OCLC group
+]
+
+gls.update(set(non_global_excludes))
+
 # Walk through ML store and create 16 LMDBs for Object:Subject
 ml = cfgs.results['marklogic']['recordcache']
-
 
 refcts = []
 rescts = []
@@ -38,5 +56,5 @@ for rec in ml.iter_records():
         except:
             ref_hash[x] = 1
 
-    if len(refcts) >= 500000:
+    if len(refcts) >= 1000000:
         break
