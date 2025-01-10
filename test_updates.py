116120619481381cb2061a2efbf9730d3cad06f0
Rob Sanderson <azaroth@gmail.com>
2025-01-10 15:41:06 -0500

    logic

--- test_updates.py
+++ test_updates.py
@@ -17,60 +17,9 @@ q = """SELECT ?s WHERE {
   ?s <https://lux.collections.yale.edu/ns/allRefCtr> <%%yuid%%>
 }"""
 
-h = cfgs.internal['ypm']['harvester']
-dc = cfgs.internal['ypm']['datacache']
-ypmns = cfgs.internal['ypm']['namespace']
-
-# In prod, this could be acquirer to run the mappers, store in cache and so on
-acquirer = cfgs.internal['ypm']['acquirer']
-latest = dc.latest()
-lday = latest[:10] + "T00:00:00"
-sbx = cfgs.marklogic_stores['ml_sandbox']['store']
-
-g = h.crawl(last_harvest=lday, refsonly=True)
-chgs = list(g)
-
-creates = []
-updates = []
-deletes = []
-
-chgd = {}
-for (chg, ident, empty, dt) in chgs:
-    if chg == "create":
-        creates.append(ident)
-    elif chg == "update":
-        updates.append(ident)
-    elif chg == "delete":
-        deletes.append(ident)
-    else:
-        print(f"Saw chg: {chg} for {ident} ?")
-    if not ident in chgd:
-        chgd[ident] = chg
-    else:
-        print(f"saw duplicate {ident}: {chg}, more recently {chgd[ident]}")
-
-for i in updates[:]:
-    if not i in dc:
-        creates.append(i)
-        updates.remove(i)
-        chgd[i] = "create"
-
-
-# First process creates as easy
-for ident in creates:
-    # new_rec = acquirer.acquire(ident, store=STORE_OKAY)
-
-    # reconcile, and track referenced as normal
-    # merge all (incl refs)
-    # save to batches
-    # upload batches
-
-    pass
-
-
 def _walk_refs(node, refs):
     if 'id' in node:
-        refs[node['id']] = 1
+        refs[node['id']] = node['type']
 
     for k, v in node.items():
         if not type(v) in [list, dict]:
@@ -84,15 +33,58 @@ def _walk_refs(node, refs):
         elif type(v) == dict:
             _walk_refs(v, refs)
 
+h = cfgs.internal['ypm']['harvester']
+dc = cfgs.internal['ypm']['datacache']
+ypmns = cfgs.internal['ypm']['namespace']
 
+# In prod, this could be acquirer to run the mappers, store in cache and so on
+acquirer = cfgs.internal['ypm']['acquirer']
+latest = dc.latest()
+lday = latest[:10] + "T00:00:00"
+sbx = cfgs.marklogic_stores['ml_sandbox']['store']
+creates = []
+updates = []
+deletes = []
+
+for (chg, ident, empty, dt) in h.crawl(last_harvest=lday, refsonly=True):
+    if chg == "create":
+        creates.append(ident)
+    elif chg == "update":
+        updates.append(ident)
+    elif chg == "delete":
+        deletes.append(ident)
+    else:
+        print(f"Saw chg: {chg} for {ident} ?")
+
+for i in updates[:]:
+    if not i in dc:
+        creates.append(i)
+        updates.remove(i)
+
+print("Fetching new recs")
+
+temp_recs = []
+
+# First process creates as easy
+for ident in creates:
+    new_rec = acquirer.acquire(ident, store=STORE_OKAY)
+    if not STORE_OKAY:
+        temp_recs.append(new_rec)
+
+
+print("Fetching updated recs")
+
+maybe_delete = {}
 for ident in updates:
-
     old_rec = acquirer.acquire(ident, store=STORE_OKAY)
     new_rec = acquirer.acquire(ident, store=STORE_OKAY, refetch=True)
 
     if new_rec['data'] == old_rec['data']:
         # Already seen this one, carry on
         continue
+
+    if not STORE_OKAY:
+        temp_recs.append(new_rec)
 
     # First -- is this a boring update with no reference changes
     old_refs = {}
@@ -108,19 +100,27 @@ for ident in updates:
     else:
         # referenced records changed
         removed = sold - snew
-        added = snew - sold
-        print(f"Removed: {removed} | Added: {added}")
         if not removed:
             # This is also able to be updated in place, as won't cause a delete
+            # Can add refs no problem, as going to submit all changes before querying
             continue
         else:
-            break
+            # This might cause a delete on any of these
+            for r in removed:
+                ruri = cfgs.make_qua(r, removed[r])
+                maybe_delete[ruri] = 1
 
+# Now send all updates to ML before doing any queries
+
+for ident in deletes:
+    # Can I delete ident?
+
+    # Are there any records that reference ident, that aren't also slated for deletion?
+    old_rec = acquirer.acquire(ident, store=STORE_OKAY)
     uri = old_rec['data']['id']
     typ = old_rec['data']['type']
     quri = cfgs.make_qua(uri, typ)
     yuid = idmap[quri]
-    uris = idmap[yuid]
     q2 = q.replace('%%yuid%%', yuid)
     refs = cfgs.marklogic_stores['ml_sandbox']['store'].search_sparql_ids(q2)
 
@@ -135,8 +135,15 @@ for ident in updates:
             (ref_src, ref_ident) = cfgs.split_uri(ref_uri)
             if ref_src['name'] == 'ypm':
                 # this is internal reffing record
-                changed = ref_ident in chgd
+                changed = ref_ident in deletes
 
+    # if no references, then get record's references to consider deletion and delete
 
+    # if there are references
 
-    break
+    # is there a reference that will not be deleted? If so, can't delete
+    # e.g. from another internal source, or from a global
+
+    # if only YPM or extenal references, then track
+
+    # At the end of the pass, for each record, if the set of preventing records is the a subset of the deletions, then delete
