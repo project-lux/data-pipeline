04cdab9ec78e7d55a12bf716887f7eebb5a4835c
Rob Sanderson <azaroth@gmail.com>
2025-01-10 11:49:28 -0500

    update test

--- test_updates.py
+++ test_updates.py
@@ -10,31 +10,35 @@ idmap = cfgs.get_idmap()
 cfgs.cache_globals()
 cfgs.instantiate_all()
 
-
 q = """SELECT ?s WHERE {
   ?s <https://lux.collections.yale.edu/ns/allRefCtr> <%%yuid%%>
 }"""
 
 h = cfgs.internal['ypm']['harvester']
 dc = cfgs.internal['ypm']['datacache']
+latest = dc.latest()
+lday = latest[:10] + "T00:00:00"
 sbx = cfgs.marklogic_stores['ml_sandbox']['store']
 
-g = h.crawl()
-(chg, ident, new_rec, dt) = next(g)
+g = h.crawl(last_harvest=lday, refsonly=True)
+chgs = list(g)
 
-old_rec = dc[ident]
-if not old_rec:
-    if chg == "update":
-        chg = "create"
-    elif chg == "delete":
-        print(f"Already deleted {ident}, can't continue")
-        raise ValueError()
-uri = old_rec['data']['id']
-typ = old_rec['data']['type']
-quri = cfgs.make_qua(uri, typ)
-yuid = idmap[quri]
-uris = idmap[yuid]
 
-q2 = q.replace('%%yuid%%', yuid)
-refs = cfgs.marklogic_stores['ml_sandbox']['store'].search_sparql_ids(q2)
+for (chg, ident, empty, dt) in chgs:
+    old_rec = dc[ident]
+    if not old_rec:
+        if chg == "update":
+            chg = "create"
+        elif chg == "delete":
+            print(f"Already deleted {ident}, can't continue")
+            raise ValueError()
+    uri = old_rec['data']['id']
+    typ = old_rec['data']['type']
+    quri = cfgs.make_qua(uri, typ)
+    yuid = idmap[quri]
+    uris = idmap[yuid]
 
+    q2 = q.replace('%%yuid%%', yuid)
+    refs = cfgs.marklogic_stores['ml_sandbox']['store'].search_sparql_ids(q2)
+
+    break
