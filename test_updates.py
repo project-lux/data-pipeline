14aee99b7da06dbb3f883318f8dc307b10091a7c
Rob Sanderson <azaroth@gmail.com>
2025-01-10 12:22:07 -0500

    update test

--- test_updates.py
+++ test_updates.py
@@ -16,6 +16,7 @@ q = """SELECT ?s WHERE {
 
 h = cfgs.internal['ypm']['harvester']
 dc = cfgs.internal['ypm']['datacache']
+ypmns = cfgs.internal['ypm']['namespace']
 latest = dc.latest()
 lday = latest[:10] + "T00:00:00"
 sbx = cfgs.marklogic_stores['ml_sandbox']['store']
@@ -23,12 +24,29 @@ sbx = cfgs.marklogic_stores['ml_sandbox']['store']
 g = h.crawl(last_harvest=lday, refsonly=True)
 chgs = list(g)
 
+creates = []
+updates = []
+deletes = []
+
 chgd = {}
 for (chg, ident, empty, dt) in chgs:
+    if chg == "create":
+        creates.append(ident)
+    elif chg == "update":
+        updates.append(ident)
+    elif chg == "delete":
+        deletes.append(ident)
+    else:
+        print(f"Saw chg: {chg} for {ident} ?")
     if not ident in chgd:
         chgd[ident] = chg
     else:
         print(f"saw duplicate {ident}: {chg}, more recently {chgd[ident]}")
+
+for i in updates[:]:
+    if not i in dc:
+        creates.append(i)
+        updates.remove(i)
 
 for (chg, ident, empty, dt) in chgs:
     old_rec = dc[ident]
@@ -46,5 +64,20 @@ for (chg, ident, empty, dt) in chgs:
 
     q2 = q.replace('%%yuid%%', yuid)
     refs = cfgs.marklogic_stores['ml_sandbox']['store'].search_sparql_ids(q2)
+
+    arefs = []
+    for r in refs:
+        ar = idmap[r]
+        for a in ar:
+            if a.startswith('__'):
+                continue
+            # get source + ident
+            (ref_uri, ref_typ) = cfgs.split_qua(a)
+            (ref_src, ref_ident) = cfgs.split_uri(ref_uri)
+            if ref_src['name'] == 'ypm':
+                # this is internal reffing record
+                changed = ref_ident in chgd
+
+
 
     break
