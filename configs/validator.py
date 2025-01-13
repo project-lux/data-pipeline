8dc775bb7d37b572d6ba399ac956cd343cc6ad6d
Kelly Davis <kelly.davis@yale.edu>
2025-01-13 14:02:33 -0500

    add docstring to validator

--- configs/validator.py
+++ configs/validator.py
@@ -2,12 +2,20 @@ import json
 import os
 from jsonschema import validate, ValidationError, SchemaError
 
+"""
+This script validates JSON files in the current directory against a JSON schema.
 
-# Load the JSON schema
+The script performs the following steps:
+1. Loads a JSON schema from a file named `schema.json`.
+2. Iterates through all JSON files in the current directory, excluding `schema.json` and `base_download.json`.
+3. Validates each JSON file against the schema using the `jsonschema` library.
+4. Prints whether each file is valid or provides details of any errors encountered.
+
+"""
+
+
 with open('schema.json', 'r') as schema_file:
     schema = json.load(schema_file)
-
-# Load the JSON data to be validated
 
 current_dir = os.path.dirname(os.path.abspath(__file__))
 for filename in os.listdir(current_dir):
