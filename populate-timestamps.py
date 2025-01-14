407c03c1d0c49b069b68a4662d2b9e5bfedc8758
Kelly Davis <kelly.davis@yale.edu>
2025-01-14 10:20:03 -0500

    overwrite log every time

--- populate-timestamps.py
+++ populate-timestamps.py
@@ -14,7 +14,7 @@ logging.basicConfig(
     level=logging.INFO,
     format='%(asctime)s - %(levelname)s - %(message)s',
     handlers=[
-        logging.FileHandler("populate-timestamps.log"),
+        logging.FileHandler("populate-timestamps.log","w"),
         logging.StreamHandler()
     ]
 )
