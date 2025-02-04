ef5c824ee81e2e2371287e36deb841345f3b5819
Kelly Davis <kelly.davis@yale.edu>
2025-02-04 15:47:42 -0500

    fix print statement

--- pipeline/sources/authorities/fast/mapper.py
+++ pipeline/sources/authorities/fast/mapper.py
@@ -68,7 +68,7 @@ class FastMapper(Mapper):
         tags = ["148","100","150","151","155","110","111","147"]
         for tag in tags:
             if root.find(f".//mx:datafield[@tag='{tag}']", self.nss) is not None:
-                print(f"Found tag {tag} in record: {etree.tostring(root, pretty_print=True).decode()[:500]}")
+                print(f"Found tag {tag} in record")
                 return self.nameTypeMap.get(tag)
         return None
         
