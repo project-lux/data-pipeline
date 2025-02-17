72abc4bfcfdb7428b874e7b251584b54f376e173
Rob Sanderson <azaroth@gmail.com>
2025-02-17 11:53:44 -0500

    -debug

--- post-build-portal.py
+++ post-build-portal.py
@@ -80,7 +80,6 @@ def process_recids(recids, thr):
     for recid in recids:
         try:
             rec = merged[recid]
-            sys.stdout.write(f"{thr}"); sys.stdout.flush()
             if not rec:
                 continue
             walk_for_refs(rec['data'], 1, local_dists, local_added, top=True)
