61b9d0f47dded62d976a715ab614204949133e9d
Rob Sanderson <azaroth@gmail.com>
2025-02-17 10:15:19 -0500

    30 threads

--- post-build-portal.py
+++ post-build-portal.py
@@ -2,6 +2,7 @@ import os
 import sys
 from dotenv import load_dotenv
 from pipeline.config import Config
+import concurrent.futures
 
 load_dotenv()
 basepath = os.getenv("LUX_BASEPATH", "")
@@ -20,15 +21,15 @@ rc = src['recordcache2']
 merged = cfgs.results['merged']['recordcache']
 
 
-def walk_for_refs(node, distance, top=False):
+def walk_for_refs(node, distance, distances, added, top=False):
 
     if not top and "id" in node and not node["id"].startswith("_"):
         yuid = node['id'].rsplit('/', 1)[-1]
-        dist = all_distances.get(yuid, 100)
+        dist = distances.get(yuid, 100)
         if distance < dist:
-            all_distances[yuid] = distance
+            distances[yuid] = distance
         if dist == 100:
-            added_refs[yuid] = distance
+            added[yuid] = distance
 
     for k, v in node.items():
         if k in ["equivalent", "access_point", "conforms_to"]:
@@ -36,9 +37,9 @@ def walk_for_refs(node, distance, top=False):
         if type(v) == list:
             for vi in v:
                 if type(vi) == dict:
-                    walk_for_refs(vi, distance)
+                    walk_for_refs(vi, distance, distances, added)
         elif type(v) == dict:
-            walk_for_refs(v, distance)
+            walk_for_refs(v, distance, distances, added)
 
 all_distances = {}
 added_refs = {}
@@ -56,21 +57,50 @@ for k in rc.iter_keys():
     if not x % 250000:
         print(f"{x}/{ttl}")
 
-print("dist=0")
-x = 0
-# populate all distance 1 references into all_distances
-for k in list(all_distances.keys()):
-    try:
-        rec = merged[k]
-        if not rec:
-            continue
-        walk_for_refs(rec['data'], 1, top=True)
-    except KeyError:
-        missing[k] = 1
-        print(f"missing: {k}")
-    x += 1
-    if not x % 50000:
-        print(f"{x}/{ttl} - {len(added_refs)} new")
+
+def process_recids(recids):
+    local_dists = {}
+    local_added = {}
+    for recid in recids:
+        try:
+            rec = merged[recid]
+            if not rec:
+                continue
+            walk_for_refs(rec['data'], 1, local_dists, local_added, top=True)
+        except KeyError:
+            missing[k] = 1
+            print(f"missing: {recid}")
+    return [local_dists, local_added]
+
+
+print("dist=0, 30 threads")
+recids = list(all_distances.keys())
+future_dists = {}
+with concurrent.futures.ThreadPoolExecutor(max_workers=30) as executor:
+    for x in range(30):
+        future = executor.submit(process_recids, recids[x::30])
+        future_dists[future] = x
+    for future in concurrent.futures.as_completed(future_dists):
+        (local_dists, local_added) = future.result()
+        all_distances.update(local_dists)
+        added_refs.update(local)
+
+
+# print("dist=0")
+# x = 0
+# # populate all distance 1 references into all_distances
+# for k in list(all_distances.keys()):
+#     try:
+#         rec = merged[k]
+#         if not rec:
+#             continue
+#         walk_for_refs(rec['data'], 1, all_distances, added_refs, top=True)
+#     except KeyError:
+#         missing[k] = 1
+#         print(f"missing: {k}")
+#     x += 1
+#     if not x % 50000:
+#         print(f"{x}/{ttl} - {len(added_refs)} new")
 
 print("Added refs")
 x = 0
