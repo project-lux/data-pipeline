c784cfb1e5bb59767207cebb17ad907b835352ee
Rob Sanderson <azaroth@gmail.com>
2025-04-08 15:05:35 -0400

    tidier ray example code

--- ray-test2.py
+++ ray-test2.py
@@ -10,13 +10,11 @@ import sys
 class LoggingActor:
     def __init__(self):
         self.logs = {}
-
     def add_to_log(self, which, txt):
         try:
             self.logs[which].append(txt)
         except:
             self.logs[which] = [txt]
-
     def get_log(self, which):
         try:
             l = self.logs[which]
@@ -26,30 +24,24 @@ class LoggingActor:
             self.logs[which] = []
             return []
 
-
 class Process:
-
-    def __init__(self):
-        self.comms = None
 
     @ray.remote
     def squares(self, x, log):
         for y in range(random.randint(5, 10)):
             log.add_to_log.remote(x, f"Log from {x}/{y}")
-            time.sleep(0.5)
+            time.sleep(0.3 + (random.randint(1,6) / 10))
         return x*x
 
-    def process(self, log):
-
+    def process(self, log, max_workers):
             print("Starting")
-            futures = [self.squares.remote(self, i, log) for i in range(5)]
+            futures = [self.squares.remote(self, i, log) for i in range(max_workers)]
             while futures:
                 ready_refs, futures = ray.wait(futures, num_returns=1, timeout=0.5)
-
-                for x in range(5):
+                for x in range(max_workers):
                     resp = ray.get(log.get_log.remote(x))
-                    print(resp)
-
+                    for r in resp:
+                        print(r)
                 if ready_refs:
                     res = ray.get(ready_refs[0])
                     if type(res) == int:
@@ -57,9 +49,7 @@ class Process:
 
 if __name__ == "__main__":
     ray.init(log_to_driver=False)
-
     log_actor = LoggingActor.remote()
-
+    max_workers = 10
     p = Process()
-    p.process(log_actor)
-
+    p.process(log_actor, max_workers)
