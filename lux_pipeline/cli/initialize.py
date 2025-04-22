d447db1adf9f37813b32420c681fe0da47e1c271
Rob Sanderson <azaroth@gmail.com>
2025-04-22 14:11:02 -0400

    last few cli refactors

--- lux_pipeline/cli/initialize.py
+++ lux_pipeline/cli/initialize.py
@@ -7,69 +7,72 @@ import gzip
 import tarfile
 import subprocess
 
+from ._handler import BaseHandler as BH
 
-def handle_command(cfgs, args, rest):
-    # note that cfgs will be None
-    if not rest:
-        rest = ['.']
-    where = os.path.abspath(rest[0])
-    os.chdir(where)
+class CommandHandler(BH):
 
-    p = Path(where)
-    p.mkdir(parents=True, exist_ok=True)
-    (p / 'files').mkdir()
-    (p / 'logs/flags').mkdir(parents=True)
-    (p / 'indexes').mkdir()
-    (p / 'input').mkdir()
-    (p / 'output').mkdir()
-    (p / 'system_config').mkdir()
-    (p / 'temp').mkdir()
+    def process(self, args, rest):
+        # note that cfgs will be None
+        if not rest:
+            rest = ['.']
+        where = os.path.abspath(rest[0])
+        os.chdir(where)
 
-    # Try to use git clone to fetch main
-    # Then it can stay in sync
-    try:
-        result = subprocess.run(["git", "clone", "https://github.com/project-lux/pipeline-configs.git"])
-        worked = True
-    except:
-        worked = False
-    if not (p / "pipeline-configs").exists():
-        worked = False
-    else:
-        result = subprocess.run(["cp", "-r", "pipeline-configs/configs/system/config_cache", "system_config"])
+        p = Path(where)
+        p.mkdir(parents=True, exist_ok=True)
+        (p / 'files').mkdir()
+        (p / 'logs/flags').mkdir(parents=True)
+        (p / 'indexes').mkdir()
+        (p / 'input').mkdir()
+        (p / 'output').mkdir()
+        (p / 'system_config').mkdir()
+        (p / 'temp').mkdir()
 
-    if not worked:
-        # No git? Then download a release
-        # populate the configurations from github
+        # Try to use git clone to fetch main
+        # Then it can stay in sync
+        try:
+            result = subprocess.run(["git", "clone", "https://github.com/project-lux/pipeline-configs.git"])
+            worked = True
+        except:
+            worked = False
+        if not (p / "pipeline-configs").exists():
+            worked = False
+        else:
+            result = subprocess.run(["cp", "-r", "pipeline-configs/configs/system/config_cache", "system_config"])
 
-        (p / 'pipeline-configs').mkdir()
-        g = Github()
-        repo = g.get_repo('project-lux/pipeline-configs')
-        rel = repo.get_latest_release()
-        url = rel.tarball_url
-        r = requests.get(url)
-        tdata = gzip.decompress(r.content)
-        tf = tarfile.TarFile(fileobj=io.BytesIO(tdata))
+        if not worked:
+            # No git? Then download a release
+            # populate the configurations from github
 
-        for member in tf.getmembers(path="pipeline-configs"):
-            cn = member.name
-            cf = cn.find('configs/')
-            if cf > 0:
-                member.name = cn[cf:]
-                tf.extract(member)
-        subprocess.run(["mv", "pipeline-configs/configs/system/config_cache", "system_config"])
+            (p / 'pipeline-configs').mkdir()
+            g = Github()
+            repo = g.get_repo('project-lux/pipeline-configs')
+            rel = repo.get_latest_release()
+            url = rel.tarball_url
+            r = requests.get(url)
+            tdata = gzip.decompress(r.content)
+            tf = tarfile.TarFile(fileobj=io.BytesIO(tdata))
 
-    # Now the baseline configs are there
-    # Need to update base_dir on system config to p
-    with open('system_config/config_cache/system.json') as fh:
-        data = fh.read()
-    data = data.replace('path-to-where-files-should-live', str(p))
-    with open('system_config/config_cache/system.json', 'w') as fh:
-        fh.write(data)
+            for member in tf.getmembers(path="pipeline-configs"):
+                cn = member.name
+                cf = cn.find('configs/')
+                if cf > 0:
+                    member.name = cn[cf:]
+                    tf.extract(member)
+            subprocess.run(["mv", "pipeline-configs/configs/system/config_cache", "system_config"])
 
-    # create a .env file pointing to the system config
-    # in the current directory?
-    with open('.env', 'w') as fh:
-        fh.write(f"LUX_BASEPATH={p / 'system_config'}")
+        # Now the baseline configs are there
+        # Need to update base_dir on system config to p
+        with open('system_config/config_cache/system.json') as fh:
+            data = fh.read()
+        data = data.replace('path-to-where-files-should-live', str(p))
+        with open('system_config/config_cache/system.json', 'w') as fh:
+            fh.write(data)
 
-    print("Installed basic configuration and file system layout")
-    print("Please make sure that postgres and redis are available and configured via the files in system_config/config_cache")
+        # create a .env file pointing to the system config
+        # in the current directory?
+        with open('.env', 'w') as fh:
+            fh.write(f"LUX_BASEPATH={p / 'system_config'}")
+
+        print("Installed basic configuration and file system layout")
+        print("Please make sure that postgres and redis are available and configured via the files in system_config/config_cache")
--- lux_pipeline/cli/interactive.py
+++ lux_pipeline/cli/interactive.py
@@ -3,6 +3,8 @@ from rich import pretty
 import readline
 import rlcompleter
 import os
+from ._handler import BaseHandler as BH
+
 
 
 class LUXREPL(code.InteractiveConsole):
@@ -13,26 +15,29 @@ class LUXREPL(code.InteractiveConsole):
         else:
             return super().runsource(source, filename, symbol)
 
-def handle_command(cfgs, args, rest):
-    # Nicer print and inspect
-    from rich import print, inspect
-    pretty.install()
 
-    # Enable command line history
-    fn = os.path.expanduser("~/.python_history")
-    if os.path.exists(fn):
-        readline.read_history_file(fn)
+class CommandHandler(BH):
 
-    # Get idmap into locals
-    idmap = cfgs.get_idmap()
-    vars = globals()
-    vars.update(locals())
+    def process(self, args, rest):
+        # Nicer print and inspect
+        from rich import print, inspect
+        pretty.install()
 
-    # Enable tab completion
-    readline.set_completer(rlcompleter.Completer(vars).complete)
-    readline.parse_and_bind("bind ^I rl_complete")
-    readline.parse_and_bind("tab: complete")
+        # Enable command line history
+        fn = os.path.expanduser("~/.python_history")
+        if os.path.exists(fn):
+            readline.read_history_file(fn)
 
-    # Launch the console
-    LUXREPL(vars).interact(banner="LUX Python Interactive Console")
+        # Get idmap into locals
+        idmap = cfgs.get_idmap()
+        vars = globals()
+        vars.update(locals())
+
+        # Enable tab completion
+        readline.set_completer(rlcompleter.Completer(vars).complete)
+        readline.parse_and_bind("bind ^I rl_complete")
+        readline.parse_and_bind("tab: complete")
+
+        # Launch the console
+        LUXREPL(vars).interact(banner="LUX Python Interactive Console")
 
