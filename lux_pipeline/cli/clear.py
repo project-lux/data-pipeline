752f4a08d15502f54fb16b5b48c9a081cf7498a0
Rob Sanderson <azaroth@gmail.com>
2025-05-28 13:26:55 -0400

    counts and clears

--- lux_pipeline/cli/clear.py
+++ lux_pipeline/cli/clear.py
@@ -1,30 +1,84 @@
 from ._handler import BaseHandler as BH
 
-class CommandHandler(BH):
 
+class CommandHandler(BH):
     def add_args(self, ap):
         ap.add_argument("--cache", type=str, help="Types of cache separated by commas, or 'all'")
+        ap.add_argument("--map", type=str, help="Name of map to clear, no comma separation")
+        ap.add_argument("--index", type=str, help="Index sources separated by commas, or 'all'")
+        ap.add_argument("--force", action="store_true")
 
     def process(self, args, rest):
-
         super().process(args, rest)
         cfgs = self.configs
+
+        if args.map:
+            # Get the map from config
+            map = args.map.strip()
+            if not map:
+                print("ERROR: Map name cannot be empty")
+                return False
+            elif map not in cfgs.map_stores:
+                print(f"ERROR: Map '{map}' not found")
+                return False
+            else:
+                if map == "idmap" and not args.force:
+                    print("ERROR: Cannot clear idmap without --force")
+                    return False
+                else:
+                    cfgs.instantiate_map(map)["store"].clear()
+                    print(f"Cleared map '{map}'")
+                    return True
+
+        if args.index:
+            # Get the indexes from the source(s) from config
+            indexes = []
+            if args.index == "all":
+                # need to walk and look for them :/
+                for cfg_list in [cfgs.internal, cfgs.external, cfgs.results]:
+                    for cfg in cfg_list.values():
+                        if "indexes" in cfg:
+                            for idx in cfg["indexes"].values():
+                                idx["source"] = cfg["name"]
+                                indexes.append(idx)
+            else:
+                idx_srcs = args.index.split(",")
+                for s in idx_srcs:
+                    if s in cfgs.internal:
+                        cfg = cfgs.internal[s]
+                    elif s in cfgs.external:
+                        cfg = cfgs.external[s]
+                    elif s in cfgs.results:
+                        cfg = cfgs.results[s]
+                    else:
+                        raise ValueError(f"Unknown index source: {s}")
+                    if "indexes" in cfg:
+                        for idx in cfg["indexes"].values():
+                            idx["source"] = cfg["name"]
+                            indexes.append(idx)
+            for idx in indexes:
+                lmidx = idx["index"]
+                # lmidx.open("w")
+                # lmidx.index.drop()
+                # lmidx.close()
+                print(f"FIXME: Can't clear {idx['source']}/{idx['name']} directly? Just delete the files.")
+            return True
 
         if not args.source:
             print("Which source(s) to clear must be given")
             return False
         elif args.source == "all":
             sources = [*cfgs.internal.keys(), *cfgs.external.keys(), *cfgs.results.keys()]
-        elif args.source in ['internal', 'external', 'results']:
+        elif args.source in ["internal", "external", "results"]:
             sources = list(getattr(cfgs, args.source).keys())
         else:
-            sources = args.source.split(',')
+            sources = args.source.split(",")
 
-        if not hasattr(args, 'cache') or not args.cache:
+        if not hasattr(args, "cache") or not args.cache:
             # default to non data caches
             caches = ["recordcache", "reconciledRecordcache", "recordcache2"]
         else:
-            caches = args.cache.split(',')
+            caches = args.cache.split(",")
 
         for s in sources:
             if s in cfgs.internal:
@@ -33,7 +87,7 @@ class CommandHandler(BH):
                 cfg = cfgs.external[s]
             elif s in cfgs.results:
                 cfg = cfgs.results[s]
-            elif s in ['all_refs', 'done_refs', 'networkmap']:
+            elif s in ["all_refs", "done_refs", "networkmap"]:
                 refs = cfgs.instantiate_map(s)["store"]
                 print(f"Clearing map {s}")
                 refs.clear()
@@ -43,6 +97,6 @@ class CommandHandler(BH):
                 print(f"Could not find cache to clear: {s}")
             for c in caches:
                 cache = cfg.get(c, None)
-                if cache is not None and hasattr(cache, 'clear'):
+                if cache is not None and hasattr(cache, "clear"):
                     print(f"Clearing {c} for {s} ")
                     cache.clear()
--- lux_pipeline/cli/counts.py
+++ lux_pipeline/cli/counts.py
@@ -5,6 +5,7 @@ class CommandHandler(BH):
     def add_args(self, ap):
         ap.add_argument("--cache", type=str, help="Types of cache separated by commas, or 'all'")
         ap.add_argument("--map", type=str, help="Types of map separated by commas, or 'all'")
+        ap.add_argument("--index", type=str, help="Index sources separated by commas, or 'all'")
 
     def process(self, args, rest):
         super().process(args, rest)
@@ -17,7 +18,34 @@ class CommandHandler(BH):
         else:
             maps = args.map.split(",")
 
-        if not args.source and maps:
+        indexes = []
+        if not hasattr(args, "index") or not args.index:
+            pass
+        elif args.index == "all":
+            # need to walk and look for them :/
+            for cfg_list in [cfgs.internal, cfgs.external, cfgs.results]:
+                for cfg in cfg_list.values():
+                    if "indexes" in cfg:
+                        for idx in cfg["indexes"].values():
+                            idx["source"] = cfg["name"]
+                            indexes.append(idx)
+        else:
+            idx_srcs = args.index.split(",")
+            for s in idx_srcs:
+                if s in cfgs.internal:
+                    cfg = cfgs.internal[s]
+                elif s in cfgs.external:
+                    cfg = cfgs.external[s]
+                elif s in cfgs.results:
+                    cfg = cfgs.results[s]
+                else:
+                    raise ValueError(f"Unknown index source: {s}")
+                if "indexes" in cfg:
+                    for idx in cfg["indexes"].values():
+                        idx["source"] = cfg["name"]
+                        indexes.append(idx)
+
+        if not args.source and (maps or indexes):
             sources = []
         elif not args.source or args.source == "all":
             sources = [*cfgs.internal.keys(), *cfgs.external.keys(), *cfgs.results.keys()]
@@ -43,6 +71,7 @@ class CommandHandler(BH):
                 # uhh...
                 print(f"Could not find cache to count: {s}")
                 continue
+
             for c in caches:
                 cache = cfg.get(c, None)
                 if cache is not None:
@@ -70,7 +99,17 @@ class CommandHandler(BH):
                         store = cfgs.instantiate_map(m)["store"]
                         n = len(store)
                     total += n
-                    ttl = f"{m.rjust(16)}/records:".ljust(36)
+                    ttl = f"{m.rjust(16)}:".ljust(36)
                     print(f"{ttl} {n}")
             ttl = "Total Map Records Seen:".rjust(36)
+            print(f"{ttl} ~{total}")
+
+        if indexes:
+            for idx in indexes:
+                idx["index"].open()
+                n = len(idx["index"])
+                total += n
+                ttl = f"{idx['source'].rjust(16)}/{idx['name']}:".ljust(36)
+                print(f"{ttl} {n}")
+            ttl = "Total Index Records Seen:".rjust(36)
             print(f"{ttl} ~{total}")
