fa9b024b556fcf4a4fc76aec2317f8efc825df93
Rob Sanderson <azaroth@gmail.com>
2025-05-29 11:50:00 -0400

    Simplify BNF, fix 'b' binding over ssh

--- lux_pipeline/cli/interactive.py
+++ lux_pipeline/cli/interactive.py
@@ -1,29 +1,32 @@
 import code
 from rich import pretty
-import readline
-import rlcompleter
 import os
 from ._handler import BaseHandler as BH
 
-class LUXREPL(code.InteractiveConsole):
+try:
+    import readline
+except Exception:
+    readline = None
 
-    def runsource(self, source, filename='<input>', symbol='single'):
-        if source.startswith('??'):
+
+class LUXREPL(code.InteractiveConsole):
+    def runsource(self, source, filename="<input>", symbol="single"):
+        if source.startswith("??"):
             print(f"Intercept question: {source[2:].strip()}")
         else:
             return super().runsource(source, filename, symbol)
 
 
 class CommandHandler(BH):
-
     def process(self, args, rest):
         # Nicer print and inspect
         from rich import print, inspect
+
         pretty.install()
 
         # Enable command line history
         fn = os.path.expanduser("~/.python_history")
-        if os.path.exists(fn):
+        if readline is not None and os.path.exists(fn):
             readline.read_history_file(fn)
 
         # Get idmap into locals
@@ -33,10 +36,12 @@ class CommandHandler(BH):
         vars.update(locals())
 
         # Enable tab completion
-        readline.set_completer(rlcompleter.Completer(vars).complete)
-        readline.parse_and_bind("bind ^I rl_complete")
-        readline.parse_and_bind("tab: complete")
+        if readline is not None:
+            readline.set_completer(rlcompleter.Completer(vars).complete)
+            if "libedit" in readline.__doc__:
+                readline.parse_and_bind("bind ^I rl_complete")
+            else:
+                readline.parse_and_bind("tab: complete")
 
         # Launch the console
         LUXREPL(vars).interact(banner="LUX Python Interactive Console")
-
--- lux_pipeline/sources/bnf/loader.py
+++ lux_pipeline/sources/bnf/loader.py
@@ -1,32 +1,24 @@
 from lux_pipeline.process.base.loader import Loader
-
 import logging
-
-logger = logging.getLogger("lux_pipeline")
-
-# tar.gz files of xml files
-# each of which has a single root element rdf:RDF
-# and contains child elements of rdf:Description
-# We only want <rdf:Description rdf:about="http://data.bnf.fr/ark:/12148/*">
 
 
 class BnfLoader(Loader):
+    """Takes a tar.gz file of xml files
+    each of which has a single root element (rdf:RDF)
+    and contains child elements of (rdf:Description)
+    We only want the attribute rdf:about="http://data.bnf.fr/ark:/12148/*"""
+
     def make_identifier(self, value):
         return None
 
     def extract_identifier(self, data):
         """Extract the identifier from the raw string"""
-        rec = data["data"]
-        rec = rec.strip()
+        rec = data["data"].strip()
         fl = rec.split("\n")[0].split()[-1]
         if not fl.startswith("rdf:about"):
-            # print(f"More complicated extractor needed: {fl}")
-            # self.manager.log(logging.ERROR, f"More complicated extractor needed: {fl}")
-            return None
+            self.manager.log(logging.ERROR, f"More complicated extractor needed: {fl}")
         else:
             ident = fl.replace('rdf:about="', "").replace('">', "")
-            # self.manager.log(logging.DEBUG, f"Extracted URI: {ident}")
             if "data.bnf.fr/ark" in ident:
                 return ident.replace("http://data.bnf.fr/ark:/12148/", "")
-            else:
-                return None
+        return None
