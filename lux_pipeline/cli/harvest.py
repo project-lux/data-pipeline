9293f6fac95f53392953656831c3772ab79a72a5
Rob Sanderson <azaroth@gmail.com>
2025-05-30 12:35:42 -0400

    make until cli arg

--- lux_pipeline/cli/harvest.py
+++ lux_pipeline/cli/harvest.py
@@ -6,6 +6,7 @@ class CommandHandler(CH):
     def add_args(self, ap):
         ap.add_argument("--no-overwrite", action="store_false", help="Do not overwrite existing files/records")
         ap.add_argument("--two-phase", action="store_true", help="Process in two phases")
+        ap.add_argument("--until", type=str, help="ISO8601 date to harvest backwards to")
 
     def make_manager(self, wks, args):
         return HarvestManager(self.configs, wks)
