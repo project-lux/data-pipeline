f5ab528935337d2c41b59331646f3751fa396887
Rob Sanderson <azaroth@gmail.com>
2025-09-16 15:16:54 -0400

    shell caller

--- harvest_parallel.sh
+++ harvest_parallel.sh
@@ -4,10 +4,10 @@ if [ ! -n "$1" ]
 then
   echo "Usage: `basename $0` --[source]"
   exit 0
-fi 
+fi
 
-for count in `seq 0 49`;
+for count in `seq 0 99`;
 do
     echo $count
-    nohup python ./run-harvest.py $count 50 $1 > ../data/logs/harvest_$count$1.txt &
+    nohup python ./run-harvest.py $count 100 --pages $1 > ../data/logs/harvest_$count$1.txt &
 done
