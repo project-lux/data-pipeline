0a883be7601aa89c66b5ca8957faa6f870fbc9c5
Rob Sanderson <azaroth@gmail.com>
2025-09-18 11:02:59 -0400

    Update exportExternal.py

--- exportExternal.py
+++ exportExternal.py
@@ -13,6 +13,8 @@ idmap = cfgs.get_idmap()
 cfgs.cache_globals()
 cfgs.instantiate_all()
 
+ALL_DATA = "--all-data" in sys.argv
+
 to_do = []
 if "--all" in sys.argv:
     to_do = list(cfgs.external.items())
@@ -20,6 +22,7 @@ else:
     for src, cfg in cfgs.external.items():
         if f"--{src}" in sys.argv:
             to_do.append((src, cfg))
+
 
 if len(sys.argv) > 2 and sys.argv[1].isnumeric() and sys.argv[2].isnumeric():
     my_slice = int(sys.argv[1])
@@ -36,9 +39,11 @@ for src, cfg in to_do:
     # iterate through slice of recordcache
     # and export the raw data from datacache
 
-    dc = cfg['datacache']
+    dc = cfg["datacache"]
     rc = cfg["recordcache"]
-    ttl = rc.len_estimate()
+    store = dc if ALL_DATA else rc
+    ttl = store.len_estimate()
+
     print(f"Exporting {ttl} records from {src}")
 
     outfn = f"/data-export/output/external/export_{src}.zip"
@@ -46,22 +51,22 @@ for src, cfg in to_do:
 
     start = time.time()
     x = 0
-    with zipfile.ZipFile(outfn, 'w', compression=zipfile.ZIP_BZIP2) as fh:
-        for ident in rc.iter_keys():
+    with zipfile.ZipFile(outfn, "w", compression=zipfile.ZIP_BZIP2) as fh:
+        for ident in store.iter_keys():
             ident = cfgs.split_qua(ident)[0]
             if ident in done:
                 continue
             x += 1
             done[ident] = 1
             rec = dc[ident]
-            outjs = {'data': rec['data']}
-            with fh.open(ident, 'w') as ffh:
+            outjs = {"data": rec["data"]}
+            with fh.open(ident, "w") as ffh:
                 outs = json.dumps(outjs, separators=(",", ":"))
-                outb = outs.encode('utf-8')
+                outb = outs.encode("utf-8")
                 ffh.write(outb)
             if not x % 25000:
-                print(f"  {x} in {time.time()-start}")
+                print(f"  {x} in {time.time() - start}")
 
     fh.close()
     end = time.time()
-    print(end-start)
+    print(end - start)
