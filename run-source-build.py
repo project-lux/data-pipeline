064cfa54d61e5cac5d2ef58921717ae3df0ad536
Rob Sanderson <azaroth@gmail.com>
2026-08-13 12:59:45 -0400

    Export NT and jsonl

--- run-source-build.py
+++ run-source-build.py
@@ -4,6 +4,7 @@ import json
 import time
 from dotenv import load_dotenv
 from pipeline.config import Config
+from pipeline.sources.lux.qlever.mapper2 import QleverMapper
 
 load_dotenv()
 basepath = os.getenv("LUX_BASEPATH", "")
@@ -34,21 +35,37 @@ sys.stdout.flush()
 
 sources = ['aat', 'ulan', 'lcsh', 'tgn']
 
-for source in sources:
-    src = cfgs.external[source]
-    print(f" *** {source} ***")
-    sys.stdout.flush()
-    in_db = src["datacache"]
-    #acq = src['acquirer']
-    out_db = src['recordcache']
-    mapper = src["mapper"]
+x = 0
+with gzip.open(f"/data-export/output/lux/nt/sources_{my_slice}.nt.gz", "wt", 1) as fh:
+    for source in sources:
+        src = cfgs.external[source]
+        print(f" *** {source} ***")
+        sys.stdout.flush()
+        in_db = src["datacache"]
+        out_db = src['recordcache']
+        mapper = src["mapper"]
+    
+        ql_mpr = QleverMapper(src)
+    
+        out_db.defer_commits(every=1000)
+        for rec in in_db.iter_records_slice(my_slice, max_slice):
+            rec2 = mapper.transform(rec, None)
+            if rec2 is not None:
+                idq = cfgs.make_qua(rec2['identifier'], rec2['data']['type'])
+                rec2['identifier'] = idq
+                out_db[idq] = rec2
+                out_db.checkpoint()
 
-    out_db.defer_commits(every=1000)
-    for rec in in_db.iter_records_slice(my_slice, max_slice):
-        rec2 = mapper.transform(rec, None)
-        if rec2 is not None:
-            idq = cfgs.make_qua(rec2['identifier'], rec2['data']['type'])
-            rec2['identifier'] = idq
-            out_db[idq] = rec2
-            out_db.checkpoint()
-    out_db.flush()
+                try:    
+                    res = ql_mpr.transform(rec2)
+                    if res:
+                        fh.write("\n".join(res))
+                        fh.write("\n")
+                except Exception as e:
+                    print(f"*** {rec2.get('yuid', '?')} failed in the qlever mapper: {e}")
+                    sys.stdout.flush()
+                x += 1
+                if not x % 100000:
+                    print(f"{x} {time.time() - start}")
+                    sys.stdout.flush()
+        out_db.flush()
