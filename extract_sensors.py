b4f1ab0f3d6996223b5eb97aca1d5f9a276ef94f
Rob Sanderson <azaroth@gmail.com>
2026-08-26 12:09:33 -0400

    Update extract_sensors.py

--- extract_sensors.py
+++ extract_sensors.py
@@ -19,6 +19,28 @@ for hmo in yuag.iter_records_type('HumanMadeObject'):
     identifiers = hmo['data']['identified_by']
     acts = hmo['data'].get('used_for', [])
     if acts:
-        print(acts)
-        print(identifiers)
+        sids = []
+        acc_no = ""
+        sys_no = ""
+        for act in acts:
+            cxns = [x['id'] for x in act.get('classified_as', [])]
+            if "http://vocab.getty.edu/aat/300379380" in cxns:
+                # found sensors
+                sensors = act.get('used_specific_object', [])
+                sids = []
+                for sensor in sensors:
+                    if 'identified_by' in sensor:
+                        sids.append(sensor['identified_by'][0]['content'])
+
+        if sids:
+            for ident in identifiers:
+                if ident['type'] == 'Identifier':   
+                    cxns = [x['id'] for x in ident.get('classified_as', [])]                
+                    if "http://vocab.getty.edu/aat/300312355" in cxns:
+                        acc_no = ident['content']
+                    elif "http://vocab.getty.edu/aat/300435704" in cxns:
+                        sys_no = ident['content']
+            print(sids)
+            print(acc_no)
+            print(sys_no)
 
