a5c4555611bbd05b2a03ddee16d072d96e949b86
Rob Sanderson <azaroth@gmail.com>
2026-09-14 11:51:19 -0400

    Remove 256 limit with new TEXT identifier column

--- pipeline/sources/museums/mds/loader.py
+++ pipeline/sources/museums/mds/loader.py
@@ -21,7 +21,7 @@ class MdsLoader(Loader):
         return None
 
     def get_identifier_json(self, js):
-        return js['id'].replace(self.config['namespace'], '')[:256]
+        return js['id'].replace(self.config['namespace'], '')
 
     def post_process_json(self, js):
         return js
