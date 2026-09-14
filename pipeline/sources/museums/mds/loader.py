d51c583797c947c62ab675b5752b958b9a4e14a5
Rob Sanderson <azaroth@gmail.com>
2026-09-14 18:15:14 -0400

    Update loader.py

--- pipeline/sources/museums/mds/loader.py
+++ pipeline/sources/museums/mds/loader.py
@@ -76,8 +76,9 @@ class MdsLoader(Loader):
                         print(l)
                         raise NotImplementedError(f"is get_identifier_raw or _json implemented for {self.__class__.__name__}?")
                     elif what in self.out_cache:
-                        print(f"{what} from {f} already in cache??")
+                        # print(f"{what} from {f} already in cache??")
                         # raise ValueError(what)
+                        pass
                     self.out_cache[what] = new
                     if not x % 50000:
                         t = time.time() - start
